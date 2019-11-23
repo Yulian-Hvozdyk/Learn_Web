6399a6d76d11da4ea01c1f7387983c2fec727384
Yulian <zhou@ZHOU-MacBook-Pro.local>
2019-11-23 19:21:33 +0100

    Random number

--- JS.js
+++ JS.js
@@ -1,12 +1,19 @@
 
+/* Выводит значение полученное из input введенное пользователем*/
 
 function showMessage () {
     alert(document.getElementById('message').value);
 }
-/* Выводит значение полученное из input введенное пользователем*/
 
+// Удаляет с input введенные элементы
 function delText () {
     alert("Проходит очистка в input с надписью");
     message.value = "";
 }
-// Удаляет с input введенные элементы
+
+// Выводит рандомное число со второго input
+function showRandomNum () {
+    let ourNumber = document.getElementById('randomNum').value;
+    let randomNumber = Math.random() * ourNumber;
+    alert(Math.floor(randomNumber));
+}
