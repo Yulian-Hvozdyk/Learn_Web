9714572ba370af4bbee6033abae8bd7d2cbd843c
Yulian <zhou@ZHOU-MacBook-Pro.local>
2019-11-23 19:46:40 +0100

    Button random number запрограмирована

--- JS.js
+++ JS.js
@@ -1,19 +1,23 @@
 
-/* Выводит значение полученное из input введенное пользователем*/
+/* Кнопка "Вывести на экран", выводит значение полученное из input введенное пользователем*/
 
 function showMessage () {
     alert(document.getElementById('message').value);
 }
 
-// Удаляет с input введенные элементы
+// Кнопка "Очистить поле", удаляет с input введенные элементы
 function delText () {
     alert("Проходит очистка в input с надписью");
     message.value = "";
 }
 
-// Выводит рандомное число со второго input
+// Кнопка "Рандомное число" генерирует рандомное число со второго input и выводит на экран
 function showRandomNum () {
     let ourNumber = document.getElementById('randomNum').value;
     let randomNumber = Math.random() * ourNumber;
-    alert(Math.floor(randomNumber));
+    if (randomNumber || "" ) {
+        alert(Math.floor(randomNumber));
+    } else {
+        alert("Это не число!");
+    }
 }
