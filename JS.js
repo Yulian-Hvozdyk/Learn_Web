ba9f7d5d090950433ca3d288818f67f043d14c8e
Yulian <zhou@ZHOU-MacBook-Pro.local>
2019-11-25 05:04:24 +0100

    Пытаюсь изменить функцию

--- JS.js
+++ JS.js
@@ -14,16 +14,27 @@ function delText () {
     alert("Проходит очистка в input с надписью");
     textOfUser.value = "";
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+//////////////////////////////////////////////////////////////////////////////////////////////
+// ФУНКЦИИ РАНДОМА
 // Кнопка "Рандомное число" генерирует рандомное число со второго input и выводит на экран
-function showRandomNum () {
-    let ourNumber = document.getElementById('randomNum').value;
-    let randomNumber = Math.random() * ourNumber;
-    if (randomNumber || "" ) {
-        alert(Math.floor(randomNumber));
+// function showRandomNum () {
+//     let ourNumber = document.getElementById('randomNum').value;
+//     let randomNumber = Math.random() * ourNumber;
+//     if (randomNumber || '') {
+//         alert(Math.floor(randomNumber));
+//     } else {
+//         alert("Это не число!");
+//     }
+// }
+
+function showRandomNum(min = 0, max) {
+    max = document.getElementById('randomNum').value;
+    randomNumber = Math.random() * (max - min ) + min;
+   if (typeof(max) ==! min) {
+        alert(document.write(showRandomNum()));
     } else {
         alert("Это не число!");
     }
+    
 }
+                     
